aa2a001992dc42cc75f25a4c99a61c135481a250
vitali funshste <vfunshte-unknown@terracottatech.com>
2012-10-03 02:36:51 +0000

    Common enums for search meta data.
    svn path=/search/trunk/; revision=21173

--- search/src/main/java/com/terracottatech/search/SearchCommand.java
+++ search/src/main/java/com/terracottatech/search/SearchCommand.java
@@ -5,6 +5,6 @@ package com.terracottatech.search;
 
 public enum SearchCommand {
 
-  PUT, REMOVE, CLEAR, REMOVE_IF_VALUE_EQUAL, PUT_IF_ABSENT, REPLACE;
+  PUT, REMOVE, CLEAR, REMOVE_IF_VALUE_EQUAL, PUT_IF_ABSENT, REPLACE, NOT_SET
 
 }
---
+++ search/src/main/java/com/terracottatech/search/SearchMetaData.java
@@ -0,0 +1,23 @@
+/*
+ * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
+ */
+package com.terracottatech.search;
+
+public enum SearchMetaData {
+  KEY("KEY@"), COMMAND("COMMAND@"), ATTR("ATTR@"), VALUE("VALUE@"), PREV_VALUE("PREV_VALUE@"), CACHENAME("CACHENAME@");
+
+  private final String tag;
+
+  SearchMetaData(String text) {
+    tag = text;
+  }
+
+  @Override
+  public String toString() {
+    return tag;
+  }
+
+  public boolean equals(String s) {
+    return toString().equals(s);
+  }
+}
