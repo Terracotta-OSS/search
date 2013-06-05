d245d24c04443304408349db160d49699ee3f534
vitali funshste <vfunshte-unknown@terracottatech.com>
2013-06-05 03:24:25 +0000

    ING-7: added initial type schema support
    svn path=/search/trunk/; revision=27897

--- search/src/main/java/com/terracottatech/search/ValueType.java
+++ search/src/main/java/com/terracottatech/search/ValueType.java
@@ -3,6 +3,10 @@
  */
 package com.terracottatech.search;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
 public enum ValueType {
   VALUE_ID,
 
@@ -33,5 +37,37 @@ public enum ValueType {
   BYTE_ARRAY,
 
   ENUM;
+
+  private static final Map<Class<?>, ValueType> MAPPINGS = new HashMap<Class<?>, ValueType>(values().length);
+
+  public static ValueType valueOf(Class<?> c) {
+    ValueType type = MAPPINGS.get(c);
+    if (type != null) { return type; }
+
+    return c.isEnum() ? ENUM : null;
+  }
+
+  static {
+    MAPPINGS.put(Boolean.class, BOOLEAN);
+    MAPPINGS.put(Byte.class, BYTE);
+    MAPPINGS.put(Character.class, CHAR);
+    MAPPINGS.put(Double.class, DOUBLE);
+    MAPPINGS.put(Float.class, FLOAT);
+    MAPPINGS.put(Integer.class, INT);
+    MAPPINGS.put(Long.class, LONG);
+    MAPPINGS.put(Short.class, SHORT);
+    MAPPINGS.put(String.class, STRING);
+    MAPPINGS.put(java.util.Date.class, DATE);
+    MAPPINGS.put(java.sql.Date.class, SQL_DATE);
+    MAPPINGS.put(char.class, CHAR);
+    MAPPINGS.put(int.class, INT);
+    MAPPINGS.put(byte[].class, BYTE_ARRAY);
+    MAPPINGS.put(long.class, LONG);
+    MAPPINGS.put(byte.class, BYTE);
+    MAPPINGS.put(boolean.class, BOOLEAN);
+    MAPPINGS.put(float.class, FLOAT);
+    MAPPINGS.put(double.class, DOUBLE);
+    MAPPINGS.put(short.class, SHORT);
+  }
 
 }
